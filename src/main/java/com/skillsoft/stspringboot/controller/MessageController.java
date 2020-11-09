df44d40645591e54ec60068b586bb6f3b4795b58
Bhagyalakshmi U <b.mattakkarau@dxc.com>
2020-11-10 00:14:15 +0530

    First Commit

---
+++ src/main/java/com/skillsoft/stspringboot/controller/MessageController.java
@@ -0,0 +1,29 @@
+package com.skillsoft.stspringboot.controller;
+
+
+
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.RequestParam;
+
+	import org.springframework.stereotype.Controller;
+	import org.springframework.ui.Model;
+	import org.springframework.beans.factory.annotation.Autowired;
+
+	@Controller
+
+	public class MessageController {
+		@Autowired
+		private MessageProperties properties;
+	     @GetMapping ("/greeting")
+	     public String greeting (@RequestParam String name,Model model)
+	     {
+	    	 String message=properties.getGreeting() + name;
+	    	 model.addAttribute("greetingMessage",message);
+	    	 
+	    	 return "homePage";
+	     }
+	     
+	     
+	     
+	     
+	}
